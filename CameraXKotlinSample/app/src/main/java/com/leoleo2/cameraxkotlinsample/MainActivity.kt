fc18e9eaa1212025ee8da55de27859e015d93725
LeoAndo <reo.ando@sun-asterisk.com>
2022-07-22 23:18:39 +0900

    比率調整

--- CameraXKotlinSample/app/src/main/java/com/leoleo2/cameraxkotlinsample/MainActivity.kt
+++ CameraXKotlinSample/app/src/main/java/com/leoleo2/cameraxkotlinsample/MainActivity.kt
@@ -10,7 +10,6 @@ import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts.*
@@ -21,11 +20,15 @@ import androidx.camera.video.*
 import androidx.camera.video.VideoCapture
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
+import androidx.window.WindowManager
 import com.leoleo2.cameraxkotlinsample.databinding.ActivityMainBinding
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
 
 
 typealias LumaListener = (luma: Double) -> Unit
@@ -200,8 +203,19 @@ class MainActivity : AppCompatActivity() {
             // Used to bind the lifecycle of cameras to the lifecycle owner
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
+            // Get screen metrics used to setup camera for full screen resolution
+            val metrics = WindowManager(this).getCurrentWindowMetrics().bounds
+            Log.d(TAG, "Screen metrics: ${metrics.width()} x ${metrics.height()}")
+            val screenAspectRatio = aspectRatio(metrics.width(), metrics.height())
+            Log.d(TAG, "Preview aspect ratio: $screenAspectRatio")
+            val rotation = viewBinding.viewFinder.display.rotation
+
             // Preview
             val preview = Preview.Builder()
+                // We request aspect ratio but no resolution
+                .setTargetAspectRatio(screenAspectRatio)
+                // Set initial target rotation
+                .setTargetRotation(rotation)
                 .build()
                 .also {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
@@ -216,16 +230,28 @@ class MainActivity : AppCompatActivity() {
                 )
                 .build()
             videoCapture = VideoCapture.withOutput(recorder)
-            imageCapture = ImageCapture.Builder().build()
-            /*
+            imageCapture = ImageCapture.Builder()
+                // We request aspect ratio but no resolution to match preview config, but letting
+                // CameraX optimize for whatever specific resolution best fits our use cases
+                .setTargetAspectRatio(screenAspectRatio)
+                // Set initial target rotation, we will have to call this again if rotation changes
+                // during the lifecycle of this use case
+                .setTargetRotation(rotation)
+                .build()
+/*
             val imageAnalyzer = ImageAnalysis.Builder()
+                // We request aspect ratio but no resolution
+                .setTargetAspectRatio(screenAspectRatio)
+                // Set initial target rotation, we will have to call this again if rotation changes
+                // during the lifecycle of this use case
+                .setTargetRotation(rotation)
                 .build()
                 .also {
                     it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
                         Log.d(TAG, "Average luminosity: $luma")
                     })
                 }
-             */
+ */
 
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
@@ -278,6 +304,25 @@ class MainActivity : AppCompatActivity() {
         return retBitmap
     }
 
+    /**
+     *  [androidx.camera.core.ImageAnalysis.Builder] requires enum value of
+     *  [androidx.camera.core.AspectRatio]. Currently it has values of 4:3 & 16:9.
+     *
+     *  Detecting the most suitable ratio for dimensions provided in @params by counting absolute
+     *  of preview ratio to one of the provided values.
+     *
+     *  @param width - preview width
+     *  @param height - preview height
+     *  @return suitable aspect ratio
+     */
+    private fun aspectRatio(width: Int, height: Int): Int {
+        val previewRatio = max(width, height).toDouble() / min(width, height)
+        if (abs(previewRatio - RATIO_4_3_VALUE) <= abs(previewRatio - RATIO_16_9_VALUE)) {
+            return AspectRatio.RATIO_4_3
+        }
+        return AspectRatio.RATIO_16_9
+    }
+
     companion object {
         private const val TAG = "CameraXApp"
         private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
@@ -290,5 +335,7 @@ class MainActivity : AppCompatActivity() {
                     add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 }
             }.toTypedArray()
+        private const val RATIO_4_3_VALUE = 4.0 / 3.0
+        private const val RATIO_16_9_VALUE = 16.0 / 9.0
     }
 }
